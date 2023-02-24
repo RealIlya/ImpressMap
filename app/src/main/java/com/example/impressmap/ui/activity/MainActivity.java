c7825bad92f639e8f3d73dd6f2497b444b57e170
Admin <hairais@mail.ru>
2023-02-24 23:41:51 +0700

    Initial commit

---
+++ app/src/main/java/com/example/impressmap/ui/activity/MainActivity.java
@@ -0,0 +1,29 @@
+package com.example.impressmap.ui.activity;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.FragmentActivity;
+
+import com.example.impressmap.databinding.ActivityMainBinding;
+import com.example.impressmap.ui.fragment.MapFragment;
+import com.google.android.material.navigation.NavigationView;
+
+
+public class MainActivity extends FragmentActivity
+{
+    private ActivityMainBinding binding;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        getSupportFragmentManager().beginTransaction()
+                                   .replace(binding.container.getId(), new MapFragment())
+                                   .commit();
+    }
+
+}
