82f1e44ca5e853b7a66c30f803e09bd52a1c4895
Admin <hairais@mail.ru>
2023-05-21 20:32:37 +0700

    UPDATE v18.1
    rest

---
+++ app/src/main/java/com/example/impressmap/adapter/addresses/useraddresses/UserAddressesAdapterViewModel.java
@@ -0,0 +1,85 @@
+package com.example.impressmap.adapter.addresses.useraddresses;
+
+import androidx.annotation.NonNull;
+import androidx.lifecycle.ViewModel;
+
+import com.example.impressmap.model.data.Address;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class UserAddressesAdapterViewModel extends ViewModel
+{
+    private final List<Address> addressesCache;
+
+    public UserAddressesAdapterViewModel()
+    {
+        this.addressesCache = new ArrayList<>();
+    }
+
+    public boolean setAddresses(@NonNull List<Address> addresses)
+    {
+        if (addresses.equals(addressesCache))
+        {
+            return false;
+        }
+
+        if (!addressesCache.isEmpty())
+        {
+            List<Address> buf = new ArrayList<>();
+
+            for (Address address : addresses)
+            {
+                boolean added = false;
+                for (Address addressCache : addressesCache)
+                {
+                    if (addressCache.equals(address))
+                    {
+                        buf.add(address);
+                        address.setSelected(addressCache.isSelected());
+                        added = true;
+                        break;
+                    }
+                }
+                if (!added)
+                {
+                    buf.add(address);
+                }
+            }
+
+            addressesCache.clear();
+            addressesCache.addAll(buf);
+        }
+        else
+        {
+            addressesCache.addAll(addresses);
+        }
+
+        return true;
+    }
+
+    public Address getAddress(int index)
+    {
+        return addressesCache.get(index);
+    }
+
+    public int getAddressesCount()
+    {
+        return addressesCache.size();
+    }
+
+    public List<Address> getSelectedAddresses()
+    {
+        List<Address> selectedAddresses = new ArrayList<>();
+
+        for (Address address : addressesCache)
+        {
+            if (address.isSelected())
+            {
+                selectedAddresses.add(address);
+            }
+        }
+
+        return selectedAddresses;
+    }
+}
